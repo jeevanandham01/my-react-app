2a2f6bc1c6ff169c6bd074a3c20554ba0fbc1f1d
jeevanandham01 <jeevanandhampalani@gmail.com>
2023-09-19 10:33:48 +0530

    new title change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         yaash school
+         yaash school react app
         </p>
         <a
           className="App-link"
